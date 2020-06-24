e975c19eb27e7f70d543f3b5c8e6afe4d907b6d4
andrea <andreatwelve@sina.cn>
2020-06-24 09:51:36 +0800

    add ovj.js

---
+++ obj.js
@@ -0,0 +1,23 @@
+let user = {};
+console.log(user);
+
+user.name = 'John';
+console.log(user.name);
+
+user.surname = 'Mike';
+console.log(user.surname);
+
+user.name = 'Peter';
+console.log(user.name);
+
+user = {};
+console.log(user);
+
+var fruit = {
+  apple: 20,
+  pear: 20,
+  peach: 10
+};
+// write your code here...
+var output = fruit.apple + fruit.pear + fruit.peach;
+console.log(output);
